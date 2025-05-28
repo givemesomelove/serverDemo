d1683fa5f2cc28f997fec4995f268741b0e52a42
lizhaohu <88468457+givemesomelove@users.noreply.github.com>
2025-05-28 14:04:13 +0800

    feat:新建项目

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/api/hello', (req, res) => {
+    res.json({
+        message: '简单的get请求!',
+        timestamp: new Date().toISOString(),
+        status: 200,
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+    console.log(`api测试地址: http://localhost:${port}/api/hello`)
+})
+
